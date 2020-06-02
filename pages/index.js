94dddda4a7500d38ef08877157a37dc1208f428b
Tobin Saunders <tobin@pixelsandwhich.co.uk>
2020-06-02 21:55:57 +0100

    first commit setup

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Heading = styled.h1`
+  color: blue;
+  border: 10px solid pink;
+  font-weight: bold;
+`;
+
+export default () => <Heading>HELLO NEXT APPpp</Heading>;
