91cf5c740f1e5a0ef4c02605f3f95b633a8c850b
Tobin Saunders <tobin@pixelsandwhich.co.uk>
2020-06-05 23:10:57 +0100

    added logo and heading

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 
-const Heading = styled.h1`
-  color: blue;
-  border: 10px solid pink;
-  font-weight: bold;
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+  `;
+
+const Grid = styled.div`
+  display: flex;
+`;
+const Header = styled.div`
+  height: 100px;
+  width: 100%;
+  align-items: center;
+  box-shadow: 0px 0px 50px 0px #ddd;
+  border-bottom: 1px solid #fff;
+  background: linear-gradient(#fff, #f9f8f8);
+  img {
+    display: flex;
+    height: 100%;
+    margin: 0 auto;
+  }
 `;
 
-export default () => <Heading>HELLO NEXT APPpp</Heading>;
+export default () => (
+  <Grid>
+    <GlobalStyle />
+    <Header>
+      <img src="/GnGlogo.svg" />
+    </Header>
+  </Grid>
+);
