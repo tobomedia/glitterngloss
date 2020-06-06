bef9780c9fccc312d8afa1a6835922e5e2d2ae70
Tobin Saunders <tobin@pixelsandwhich.co.uk>
2020-06-06 22:06:22 +0100

    try pdf

--- func/test.js
+++ func/test.js
@@ -1,6 +1,24 @@
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: "hello you2",
-  };
+var pdf = require("html-pdf");
+
+// exports.handler = async () => {
+//   const document = pdf.create("<h1>the document</h1>");
+//   return {
+//     statusCode: 200,
+//     body: document,
+//   };
+// };
+
+exports.handler = (event, context, callback) => {
+  const docDefinition = pdf.create("<h1>test document</h1>");
+  getBlob(docDefinition).then((blob) => {
+    callback(null, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/pdf",
+      },
+      statusCode: 200,
+      body: blob.toString("base64"),
+      isBase64Encoded: true,
+    });
+  });
 };
